f5a98075dceb8b465ec1657b1ba8ba3c493d7d4a
muhbahkm <muh.bahkm@gmail.com>
2026-08-11 17:46:50 +0300

    fix: disambiguate matcher import in sale return tests

--- spike/ibex_foundation/test/post_sale_return_service_test.dart
+++ spike/ibex_foundation/test/post_sale_return_service_test.dart
@@ -1,4 +1,4 @@
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide isNotNull;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ibex_foundation_spike/database/spike_database.dart';
 import 'package:ibex_foundation_spike/operating_engine/post_sale_command.dart';
