85c60fd64a303cdae4fcc2561470b3d59ab6889d
muhbahkm <muh.bahkm@gmail.com>
2026-08-11 20:47:04 +0300

    fix: avoid Drift matcher name collision in expense workflow test

--- spike/ibex_foundation/test/expense_operational_workflow_test.dart
+++ spike/ibex_foundation/test/expense_operational_workflow_test.dart
@@ -1,4 +1,4 @@
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide isNotNull;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ibex_foundation_spike/agent/approved_expense_draft_to_command.dart';
 import 'package:ibex_foundation_spike/agent/command_registry.dart';
