7b468090d592e1aca5674408a977d062fd42a347
muhbahkm <muh.bahkm@gmail.com>
2026-08-11 22:51:05 +0300

    feat: add production no-seed entrypoint

---
+++ spike/ibex_foundation/lib/main_production.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+import 'ui/ibex_production_root.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const IbexProductionRoot());
+}
