4bb1233e2f4aebb0d59474b8071cf3abea251a66
muhbahkm <muh.bahkm@gmail.com>
2026-08-11 15:14:32 +0300

    test: import Drift values for receipt fixtures

--- spike/ibex_foundation/test/receive_customer_payment_service_test.dart
+++ spike/ibex_foundation/test/receive_customer_payment_service_test.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ibex_foundation_spike/database/spike_database.dart';
 import 'package:ibex_foundation_spike/operating_engine/receive_customer_payment_command.dart';
